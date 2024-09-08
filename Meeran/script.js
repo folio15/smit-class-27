c993c321995fd4cbeddbb68ba21c67de103bf024
folio15 <azlanirfan98@gmail.com>
2024-09-08 16:48:52 +0500

    Add files via upload

---
+++ Meeran/script.js
@@ -0,0 +1,33 @@
+// var firstName  = prompt ("enter your firstname here!")
+// var lastName = prompt ("enter your lastname here!")
+// var yourAge = prompt ("enter your age here!")
+// var yourAddress = prompt ("enter your address here!")
+// alert(firstName + " "+ lastName + " "+  + " "+ yourAge +" "+ yourAddress)
+
+
+// var maths = Number (prompt("mathsmarks")) 
+// var urdu = Number (prompt("urdumarks"))
+// var english = Number (prompt("englishmarks"))
+// var physics = Number (prompt("physicsmarks"))
+// var chemistry = Number (prompt("chemistrymarks"))
+// var computer = Number (prompt("computermarks"))
+
+// var totalMarksEachSub = 100 * 6;
+// var obtainedMarks = maths + urdu + english + physics + chemistry + computer;
+// var getpercentage = obtainedMarks / totalMarksEachSub * 100
+
+// var result = "maths: " + maths + "%" + "\n" + "urdu: " + urdu + "%" + "\n" + "english: " + english + "%" + "\n" + "physics: " + physics + "%" + "\n" + "chemistry: " + chemistry + "%" + "\n" + "computer: " + computer + "%" + "\n" 
+// alert(result)
+
+var marks = Number( prompt("write obtained marks"))
+
+if(marks>= 80) {
+alert("A+ Grade")
+
+}else if(marks>= 70)
+    {console.log("A Grade")
+    }else if (marks >= 60)
+         {
+            alert("A+ Grade ")
+         }
+        
